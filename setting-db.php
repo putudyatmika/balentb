2bf64df3b20402de9da8b075cf80d1faf1a7af08
I Putu Dyatmika <pdyatmika@gmail.com>
2017-02-02 10:11:29 +0800

    laptop
    tambah font

--- setting-db.php
+++ setting-db.php
@@ -1,6 +1,7 @@
 <?php
-$url='http://192.168.1.9/balentb';
+//$url='http://192.168.1.9/balentb';
 //$url='http://10.52.6.31/balentb';
+$url='http://localhost/balentb';
 //Setting Database
 define('db_name','ntbkeg');
 define('db_user','root');
